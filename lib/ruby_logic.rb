004527d62c1afab675318e9d7895c092f866f76d
DominiqueYoumans <ddyoumans@icloud.com>
2019-12-13 12:15:14 -0800

    add passing test that shows that word contains vowels

--- lib/ruby_logic.rb
+++ lib/ruby_logic.rb
@@ -6,20 +6,26 @@ class Words
   end
 
   def word_valid(input)
-    vowels = /[aeiouy]/
-    if (vowels.scan == true)
-      "This is a vowel!"
-
-  end
-
-  def ana
-
-  end
-
-  def word_same(w_1, w_2)
-    if w_1 == w_2
+     if /[aeiouy]/.match(input)
       true
     else
       false
+    end
   end
 end
+
+#   end
+#
+#   # def ana
+#   #
+#   # end
+#
+#   # def word_same(w_1, w_2)
+#   #   if w_1 == w_2
+#   #     true
+#   #   else
+#   #     false
+#   # end
+# end
+# end
+# end
--- spec/ruby_logic_spec.rb
+++ spec/ruby_logic_spec.rb
@@ -3,7 +3,7 @@ require('ruby_logic')
 
 describe('Words') do
   it('should check if the inputted word is a word by scanning for vowels')do
-    test = Word.new()
-    expect(test.word_valid('listen')).to(eq(true))
+    test = Words.new()
+    expect(test.word_valid("listen")).to(eq(true))
   end
 end
