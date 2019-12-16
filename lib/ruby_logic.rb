0f8fde7dc3bb333281df771075a4d33415e93f52
Student <example@example.com>
2019-12-15 17:41:51 -0800

    add function to ruby_logic that will check to see if word is an Anagram or an Antigram, mostly pseudo

--- lib/ruby_logic.rb
+++ lib/ruby_logic.rb
@@ -1,11 +1,16 @@
 require 'pry'
 
 class Words
-  def initialize
+  def initialize(word_one, word_two)
     @word_one = []
     @word_two = []
   end
-
+  def word_one=(word_one)
+    @word_one = word_one
+  end
+  def word_two=(word_two)
+    @word_two = word_two
+  end
   def word_valid(input)
     if /[aeiouy]/.match(input)
       true
@@ -20,13 +25,20 @@ class Words
       false
     end
   end
-  def anagram_checker()
+  def anagram_checker(array)
+    # pseudo
+    new_array = []
+    array.each do |word_as_string|
+      new_array.push(word_as_string)
     if word_one[0] == word_two[0]
       check words at certain index
   end
   def antigram_checker()
-    if word_one[0] == word_two[0]
-      check words at certain index
+    new_array = []
+    array.each do |word_as_string|
+      new_array.push(word_as_string)
+    if word_one[0].word_as_string == word_two[0].word_as_string
+      "This is an Antigram!"
   end
 
 
