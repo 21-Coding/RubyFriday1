c8b135cfd2ed38486e70bc5feaa1702f63394aac
Student <example@example.com>
2019-12-15 17:54:30 -0800

    add more tests to correlate with specs, all specs are not passing yet

--- spec/ruby_logic_spec.rb
+++ spec/ruby_logic_spec.rb
@@ -6,7 +6,7 @@ describe('#Words')do
     test = Words.new()
     expect(test.word_valid("listen")).to(eq(true))
   end
-  it('should check if the inputted word is not a word')do
+  it('should check if the inputted word is NOT a word')do
     test = Words.new()
 expect(test.word_valid("nestil")).to(eq(false))
   end
@@ -14,8 +14,24 @@ expect(test.word_valid("nestil")).to(eq(false))
     test = Words.new()
 expect(test.word_same("listen")).to(eq("nestil"))
   end
+  it('should check if the inputted word is NOT the same word')do
+    test = Words.new()
+expect(test.word_same("listen")).to(eq("nestil"))
+  end
   it('should check if the inputted word is an anagram')do
     test = Words.new()
-expect(test.antigram_checker("listen")).to(eq("enlist"))
+expect(test.anagram_checker("listen")).to(eq("enlist"))
+  end
+  it('should check if the inputted word is NOT an anagram')do
+    test = Words.new()
+expect(test.anagram_checker("listen")).to(eq("nestil"))
+  end
+  it('should check if the inputted word is an antigram')do
+    test = Words.new()
+expect(test.antigram_checker("listen")).to(eq("silent"))
+  end
+  it('should check if the inputted word is NOT an antigram')do
+    test = Words.new()
+expect(test.antigram_checker("listen")).to(eq("nestil"))
   end
 end
