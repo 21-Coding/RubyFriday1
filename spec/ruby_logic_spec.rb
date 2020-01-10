a61c24959d633da5dc9fea65d877c91f0516eb36
Student <example@example.com>
2020-01-09 16:37:52 -0800

    add passing test showing that two words are the same

--- spec/ruby_logic_spec.rb
+++ spec/ruby_logic_spec.rb
@@ -18,18 +18,18 @@ it('should check if the inputted word is an antigram')do
 test = Word.new(@word_one, @word_two)
 expect(test.check_anti("enlist","listen")).to(eq(nil))
 end
-#   it('should check if the inputted word is the same word')do
-#     test = Word.new()
-# expect(test.word_same("listen")).to(eq("nestil"))
-#   end
+it('should check if the inputted word is NOT an anagram')do
+test = Word.new(@word_one, @word_two)
+expect(test.check_ana("listen", "nestil")).to(eq(true))
+end
+  it('should check if the inputted word is the same word')do
+    test = Word.new(@word_one, @word_two)
+expect(test.word_same("listen", "listen")).to(eq(true))
+  end
 #   it('should check if the inputted word is NOT the same word')do
 #     test = Word.new()
 # expect(test.word_same("listen")).to(eq("nestil"))
 #   end
-  it('should check if the inputted word is NOT an anagram')do
-    test = Word.new(@word_one, @word_two)
-expect(test.check_ana("listen", "nestil")).to(eq(true))
-  end
 #   it('should check if the inputted word is NOT an antigram')do
 #     test = Words.new()
 # expect(test.antigram_checker("listen")).to(eq("nestil"))
