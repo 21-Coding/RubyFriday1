ea609052e6b4319c1ef4f28c49dea4860d991047
Student <example@example.com>
2020-01-09 16:35:28 -0800

    add passing test for a string not being an anagram

--- spec/ruby_logic_spec.rb
+++ spec/ruby_logic_spec.rb
@@ -10,6 +10,14 @@ describe('#Word')do
     test = Word.new(@word_one, @word_two)
 expect(test.word_valid("8")).to(eq(false))
   end
+  it('should check if the inputted word is an anagram')do
+  test = Word.new(@word_one, @word_two)
+  expect(test.check_ana("listen", "enlist")).to(eq(true))
+end
+it('should check if the inputted word is an antigram')do
+test = Word.new(@word_one, @word_two)
+expect(test.check_anti("enlist","listen")).to(eq(nil))
+end
 #   it('should check if the inputted word is the same word')do
 #     test = Word.new()
 # expect(test.word_same("listen")).to(eq("nestil"))
@@ -18,17 +26,9 @@ expect(test.word_valid("8")).to(eq(false))
 #     test = Word.new()
 # expect(test.word_same("listen")).to(eq("nestil"))
 #   end
-  it('should check if the inputted word is an anagram')do
+  it('should check if the inputted word is NOT an anagram')do
     test = Word.new(@word_one, @word_two)
-expect(test.check_ana("listen", "enlist")).to(eq(true))
-  end
-#   it('should check if the inputted word is NOT an anagram')do
-#     test = Word.new()
-# expect(test.anagram_checker("listen")).to(eq("nestil"))
-#   end
-  it('should check if the inputted word is an antigram')do
-    test = Word.new(@word_one, @word_two)
-expect(test.check_anti("enlist","listen")).to(eq(nil))
+expect(test.check_ana("listen", "nestil")).to(eq(true))
   end
 #   it('should check if the inputted word is NOT an antigram')do
 #     test = Words.new()
